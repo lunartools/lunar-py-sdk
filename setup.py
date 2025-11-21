1ed762bec69a861b964737ce4384644b0d092627
senpai0807 <jedikun98@gmail.com>
2025-11-21 00:14:10 -0500

    Updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lunartools-sdk",
-    version="1.0.0",
+    version="1.0.1",
     description="Official SDK for Lunar Tools API",
     author="Lunar Tools",
     author_email="support@lunartools.co",
